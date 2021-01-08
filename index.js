229c6e36a2239e3a0a08c39e9f26ccc1900b68df
Rebecca Sorrell <rebeccassorrell@gmail.com>
2021-01-08 02:36:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+ return string.toUpperCase() 
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(shout){
+  console.log ("HELLO")
+}
+function logWhisper(whisper){
+  console.log ("hello")
+}
+
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+mixedCase.toLowerCase() === mixedCase // false
+mixedCase.toUpperCase() === mixedCase // false  
+
+function logWhisper (sayHiToGrandma){
+  console.log ("I can't hear you!")
+}
+
+function logShout (sayHiToGrandma){
+  console.log(YES)
+}
