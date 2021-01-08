05704cba703a155c04af9c2c8ec35b243f2fa2c7
Rebecca Sorrell <rebeccassorrell@gmail.com>
2021-01-08 02:52:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ mixedCase.toLowerCase() === mixedCase // false
 mixedCase.toUpperCase() === mixedCase // false  
 
 function sayHiToGrandma (string) {
-if (string === string.toLowerCase){
+if (string === string.toLowerCase()){
   return "I can't hear you!"
-} else if (string === string.toUpperCase){
+} else if (string === string.toUpperCase()){
   return "YES INDEED!"
 }
 }
